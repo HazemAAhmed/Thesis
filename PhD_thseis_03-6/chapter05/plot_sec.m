b3aa1979d03630a23feafac781431f99d90875da
HazemAAhmed <hazem.a.ahmed@gmail.com>
2017-11-19 23:02:27 +0100

    figures of chapter5 are finished

--- PhD_thseis_03-6/chapter05/plot_sec.m
+++ PhD_thseis_03-6/chapter05/plot_sec.m
@@ -1,10 +1,12 @@
 figure
 plot(z2,y3(1,:))
+hold on
+plot(z2,y3(2,:))
+hold on
+plot(z2,y3(3,:))
+hold on
+plot(z2,y3(4,:))
+hold on
+plot(z2,y3(5,:))
 % hold on
-% plot(z2,y3(2,:))
-% hold on
-% plot(z2,y3(3,:))
-% hold on
-% plot(z2,y3(4,:))
-% hold on
-% plot(z2,y3(5,:))
+% plot(z2,y5(6,:))
