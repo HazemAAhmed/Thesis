072643bb9c6ce5ed29d1dce204c0dd6e251696d2
HazemAAhmed <hazem.a.ahmed@gmail.com>
2017-12-03 21:15:39 +0100

    chapter4 3-12-17

--- PhD_thseis_03-6/chapter04/plot_sec.m
+++ PhD_thseis_03-6/chapter04/plot_sec.m
@@ -1,9 +1,9 @@
 figure
-plot(z2,y5(1,:))
-hold on
-plot(z2,y5(2,:))
-hold on
-plot(z2,y5(3,:))
+plot(z2,y3(6,:))
+% hold on
+% plot(z2,y5(2,:))
+% hold on
+% plot(z2,y5(3,:))
 % hold on
 % plot(z2,y3(4,:))
 % hold on
