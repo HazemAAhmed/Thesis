057e15cee548752828abdb8f452ae5244b89b0e0
hazem.ibrahim <hazem.ibrahim@husqvarnagroup.com>
2018-03-17 16:48:08 +0100

    cleanup master

--- PhD_thseis/PhD_thseis/Smart_systech_2014/matlab/moving_average_filter.m
+++
@@ -1,9 +0,0 @@
-Nw=10;%the length of the averaging interval
-clear('estimation_error_Vogt_avg')
-
-for n=Nw:length(estimation_error_Vogt)-1
-estimation_error_LB_avg(n-Nw+1)=sum(abs(estimation_error_LB(n-Nw+1:n)))/Nw;%moving average filter
-estimation_error_Shoute_avg(n-Nw+1)=sum(abs(estimation_error_Shoute(n-Nw+1:n)))/Nw;%moving average filter
-estimation_error_Vogt_avg(n-Nw+1)=sum(abs(estimation_error_Vogt(n-Nw+1:n)))/Nw;%moving average filter
-estimation_error_Vogt_biased_avg(n-Nw+1)=sum(abs(estimation_error_Vogt_biased(n-Nw+1:n)))/Nw;%moving average filter
-end
